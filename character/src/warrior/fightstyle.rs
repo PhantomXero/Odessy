c6ee86e385e319b8dfd0cbf2a5e389f48a92319d
Dalitso <dalitso.w.banda@gmail.com>
2025-12-07 17:31:01 +0200

    Improving the displaing of data

--- character/src/warrior/fightstyle.rs
+++ character/src/warrior/fightstyle.rs
@@ -1,9 +1,13 @@
 use super::weapon::{Weapon, WeaponInfo};
+use el_roi::read_int;
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-enum FightingStyle {
+const FIGHT_STYLE_GUIDE: &str =
+    "Fighting styles define stance bonuses and which weapon pools you can specialize in.";
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub enum FightingStyle {
     SwordsMan,
     DualBladeWelder,
     Tank,
@@ -29,6 +33,7 @@ pub struct FightingStyleInfo {
 }
 
 impl FightingStyleInfo {
+    #[allow(dead_code)]
     pub fn new() -> Self {
         let style = None;
         let weapons = WeaponInfo::new_weapons();
@@ -36,12 +41,34 @@ impl FightingStyleInfo {
         Self { style, weapons }
     }
 
+    #[allow(dead_code)]
     pub fn style(&self) -> Option<&FightingStyle> {
         self.style.as_ref()
     }
 
+    #[allow(dead_code)]
     pub fn weapons(&self) -> &[Weapon] {
         &self.weapons
+    }
+
+    pub fn from_prompt() -> Self {
+        println!("--- Fighting Style ---");
+        println!("{}", FIGHT_STYLE_GUIDE);
+        let options = style_options();
+        for (idx, style) in &options {
+            println!("{}. {}", idx, style);
+        }
+        let selection = read_int("Pick your primary style: ");
+        let style = options
+            .iter()
+            .find(|(idx, _)| *idx == selection)
+            .map(|(_, style)| *style)
+            .unwrap_or(FightingStyle::SwordsMan);
+        let weapons = weapons_for(style);
+        Self {
+            style: Some(style),
+            weapons,
+        }
     }
 }
 
@@ -62,5 +89,23 @@ impl fmt::Display for FightingStyleInfo {
                 .join(", ")
         };
         write!(f, "Style: {style} | Weapons: {weapon_list}")
+    }
+}
+
+fn style_options() -> Vec<(i32, FightingStyle)> {
+    vec![
+        (1, FightingStyle::SwordsMan),
+        (2, FightingStyle::DualBladeWelder),
+        (3, FightingStyle::Tank),
+        (4, FightingStyle::Caster),
+    ]
+}
+
+fn weapons_for(style: FightingStyle) -> Vec<Weapon> {
+    match style {
+        FightingStyle::SwordsMan => vec![Weapon::LongSword, Weapon::ShortSword],
+        FightingStyle::DualBladeWelder => vec![Weapon::DualShortSword, Weapon::DualLongSword],
+        FightingStyle::Tank => vec![Weapon::Hammer, Weapon::Axe],
+        FightingStyle::Caster => vec![Weapon::Hands],
     }
 }
